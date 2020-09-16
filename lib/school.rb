a87d1d3140b56ad3a598a45c6c34d05d62d44cf5
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-16 18:15:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,7 @@ class School
   end
 
   def add_student(student, grade)
+<<<<<<< HEAD
     @roster[grade] ||= []
     @roster[grade] << student
   end
@@ -20,6 +21,9 @@ class School
     @roster.each do |grade, students|
       students.sort!
     end
+=======
+    @roster[grade].push(student)
+>>>>>>> 9a7aa4e1cb26fb6fb1f6e3972f479903dbf67e28
   end
 
 end
